83c7b475834461f5b9ea1d1d52b6455ff26fa8bf
Anubhav Rai <anubhavrai100@gmail.com>
2026-03-08 11:11:34 +0530

    addming sitemap

---
+++ frontend/app/sitemap.ts
@@ -0,0 +1,22 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://api-monitor-seven.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://api-monitor-seven.vercel.app/login",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://api-monitor-seven.vercel.app/signup",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
